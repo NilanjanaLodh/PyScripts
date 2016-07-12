092b447549a0d5025203b484793aad49678ba92a
HermoiNil <nilanjanalodh@gmail.com>
2016-07-12 23:07:24 +0530

    create template for competitive coding

---
+++ template.cpp
@@ -0,0 +1,33 @@
+//Nilanjana Lodh
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+typedef vector<int> vi;
+typedef vector<lli> vli;
+
+const lli inf= 9000000000000;
+const lli mod= 1000000007;
+lli minimum(lli a, lli b)
+{
+    if(a<b)return a;
+    return b;
+}
+
+lli maximum(lli a, lli b)
+{
+    if(a>b)return a;
+    return b;
+}
+
+lli pow(lli a, lli b)
+{
+    if(b==0)
+        return 1;
+
+    lli ans = pow(a,b/2);
+    if(b%2==1)ans= (ans*ans)%mod;
+
+    return ans;
+}
+
